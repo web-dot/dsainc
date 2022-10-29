20c5778a791954b81b3573c3ff1f9b220f996eb9
web-dot <fedev.sudipto@gmail.com>
2022-10-29 15:57:18 +0530

    c2

--- main.c
+++ main.c
@@ -3,16 +3,63 @@
 
 int main()
 {
-    /*binary search - sorted array*/
-    int arr[14] = {2,7,9,13,18,24,37,41,54,67,73,89,91,100};
-    int n = binarySearch(arr, 0, 14, 89);
-    printf("%d", n);
+    /*unsorted array*/
+
+    int arr[14] = {50,41,49,39,24,10,28,30,16,31,15,20};
+    int sizeOfArr = sizeof(arr);
+    int sizeOfInt = sizeof(arr[0]);
+    int size = sizeOfArr/sizeOfInt;
+
+    /*insertion sort*/
+    insertion_sort(arr, size);
+    print_array(arr, size);
+
+
+    /*binary search*/
+    //int n = binary_search(arr, 0, 14, 89);
+    //printf("\n%d", n);
+
+    return 0;
 }
 
-int binarySearch(int *arr, int min, int n, int item)
+/*
+    Function -> insertion sort
+    @param: array
+    @param: size of array
+    time complexity: O()
+*/
+void insertion_sort(int arr[], int size)
+{
+    int i, key, j;
+    for(i=0; i<size; i++)
+    {
+        key = arr[i];
+        int j = i-1;
+        while(j >= 0 && arr[j] > key)
+        {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+        print_array(arr, size);
+        printf("\n");
+    }
+}
+
+
+/*
+    Function -> binary search
+    @param: array address
+    @param: minimum element index
+    @param: maximum element index
+    @param: item to search
+    @return: index of searched item
+    time complexity: O(log n)
+*/
+int binary_search(int *arr, int min, int max, int item)
 {
     int low = min;
-    int high = n;
+    int high = max;
     while(low < high)
     {
         int mid = low + (high-low)/2;
@@ -30,5 +77,16 @@ int binarySearch(int *arr, int min, int n, int item)
         high--;
     }
     return -1;
+}
+
+
+/*print function*/
+void print_array(int arr[], int size)
+{
+    int i;
+    for(i=0; i<size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
 }
 
